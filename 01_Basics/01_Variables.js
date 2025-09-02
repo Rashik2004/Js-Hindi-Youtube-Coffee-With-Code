2070854240ff1d7238de71b48ec90f142dba7d3e
Rashik Ghosh <rashikghosh2004@gmail.com>
2025-09-02 15:10:16 +0530

    added the Variables.js and dataTypes.js files

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 12306713;
+let accountEmail= "rashik.ghosh@gmail.com";
+var accountPassword = "rashik123";
+accountCity = "Jamshedpur";
+let accountState;
+//accountId=123 // this kind of declaration in allowed as it cannout ovveride the const
+console.log(accountId); //prints the desired values just like print statements;
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+accountEmail="priyanshu1918@gmail.com";
+accountPassword="priya123";
+accountCity="Bengaluru";
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as its newer version of the code
+//alert(3+3); // This will give error in nodejs in out local system, It runs on the js engine which runs in the browser;
+//console.log(3+3); console.log("Rashik") // this is a bad practice;
+console.log(3+3) //for a good coder the code readablity should be high;
+console.log("Rashik")
+
+let name ="Rashik"
+let age=12
+let IsloggedIn=false
+//range of the number datatype is nearby 2 to the power 53
+//big Int if we have very big number
+//we can use string data type in single quotes and double quotes
+//we can use boolean => for having decision like true or false like 0 and 1
+//null => standalone value
+//undefined => when we dont define the value of any variable
+//Symbols => Unique
+
+//Objects
+//typeof()=> it returns the data type of the variable;
+
+console.log(typeof undefined) // it returns the data type as undefined
+console.log(typeof null) // it returns the typeof as object
